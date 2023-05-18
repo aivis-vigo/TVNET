f15baef21e82580655099cf1ae53ba6b13d62ad5
aivis-vigo <aivisvigoreimarts@gmail.com>
2023-05-18 19:35:10 +0300

    update

---
+++ app/Console/Console.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types=1);
+
+namespace App\Console;
+
+use App\Models\Article;
+use App\Models\User;
+use App\Services\Article\Index\IndexArticleServices;
+use App\Services\Article\Show\ShowArticleRequest;
+use App\Services\Article\Show\ShowArticleService;
+use App\Services\User\Index\IndexUserServices;
+use App\Services\User\Show\ShowUserRequest;
+use App\Services\User\Show\ShowUserService;
+
+class Console
+{
+    private string $command;
+    private ?string $id;
+
+    public function __construct(array $argv)
+    {
+        $this->command = $argv[1];
+        $this->id = $argv[2] ?? null;
+    }
+
+    public function run()
+    {
+        switch ($this->command) {
+            case "articles":
+                if ($this->id != null) {
+                    $service = (new ShowArticleService());
+                    $response = $service->execute(new ShowArticleRequest($this->id));
+                    $this->showSingleArticle($response->article());
+                }
+
+                $service = (new IndexArticleServices());
+                $articles = $service->execute();
+                $this->displayArticles($articles);
+
+                exit;
+            case "users":
+                if ($this->id != null) {
+                    $service = (new ShowUserService());
+                    $response = $service->execute(new ShowUserRequest($this->id));
+                    $this->showSingleUser($response->user());
+                }
+
+                $service = (new IndexUserServices());
+                $users = $service->execute();
+                $this->displayUsers($users);
+
+                exit;
+            default:
+                echo "Invalid command!" . PHP_EOL;
+
+                exit;
+        }
+    }
+
+    public function displayArticles(array $articles)
+    {
+        echo "====================" . PHP_EOL;
+        foreach ($articles as $article) {
+            /** @var Article $article */
+            echo "->" . $article->title() . "<-" . PHP_EOL;
+            echo PHP_EOL;
+            echo $article->body() . PHP_EOL;
+            echo "====================" . PHP_EOL;
+        }
+    }
+
+    public function showSingleArticle(Article $article)
+    {
+        echo "->" . $article->title() . "<-" . PHP_EOL;
+        echo PHP_EOL;
+        echo $article->body() . PHP_EOL;
+    }
+
+    public function displayUsers(array $users)
+    {
+        echo "=====================" . PHP_EOL;
+        foreach ($users as $user) {
+            /** @var User $user */
+            echo "->" . $user->username() . "<-" . PHP_EOL;
+            echo "Name: " . $user->name() . PHP_EOL;
+            echo "e-mail: " . $user->email() . PHP_EOL;
+            echo "City: " . $user->city() . PHP_EOL;
+            echo "Job: " . $user->companyName() . PHP_EOL;
+            echo "====================" . PHP_EOL;
+        }
+    }
+
+    public function showSingleUser(User $user)
+    {
+        echo "->" . $user->username() . "<-" . PHP_EOL;
+        echo "Name: " . $user->name() . PHP_EOL;
+        echo "e-mail: " . $user->email() . PHP_EOL;
+        echo "City: " . $user->city() . PHP_EOL;
+        echo "Job: " . $user->companyName() . PHP_EOL;
+    }
+}
--- console.php
+++ console.php
@@ -1,82 +1,8 @@
 <?php declare(strict_types=1);
 
-use App\Services\Article\Index\IndexArticleServices;
-use App\Services\Article\Show\ShowArticleRequest;
-use App\Services\Article\Show\ShowArticleService;
-use App\Services\User\Index\IndexUserServices;
-use App\Services\User\Show\ShowUserRequest;
-use App\Services\User\Show\ShowUserService;
+use App\Console\Console;
 
 require_once 'vendor/autoload.php';
 
-$command = $argv[1] ?? null;
-$id = $argv[2] ?? null;
-
-switch ($command) {
-    case 'articles':
-        if ($id == null) {
-            $service = (new IndexArticleServices());
-            $articles = $service->execute();
-
-            echo "====================" . PHP_EOL;
-            foreach ($articles as $article) {
-                /** @var App\Models\Article $article */
-                echo "->" . $article->title() . "<-" . PHP_EOL;
-                echo PHP_EOL;
-                echo $article->body() . PHP_EOL;
-                echo "====================" . PHP_EOL;
-            }
-        } else {
-            echo "Not found!" . PHP_EOL;
-        }
-        break;
-    case 'article':
-        $service = (new ShowArticleService());
-        $response = $service->execute(new ShowArticleRequest($id));
-        $article = $response->article();
-
-        echo "====================" . PHP_EOL;
-        echo "Article Nr. $id" . PHP_EOL;
-        echo PHP_EOL;
-        echo "->" . $article->title() . "<-" . PHP_EOL;
-        echo PHP_EOL;
-        echo $article->body() . PHP_EOL;
-        echo "====================" . PHP_EOL;
-
-        break;
-    case 'users':
-        if ($id == null) {
-            $service = (new IndexUserServices());
-            $users = $service->execute();
-
-            echo "=====================" . PHP_EOL;
-            foreach ($users as $user) {
-                /** @var App\Models\User $user */
-                echo "->" . $user->username() . "<-" . PHP_EOL;
-                echo "Name: " . $user->name() . PHP_EOL;
-                echo "e-mail: " . $user->email() . PHP_EOL;
-                echo "City: " . $user->city() . PHP_EOL;
-                echo "Job: " . $user->companyName() . PHP_EOL;
-                echo "====================" . PHP_EOL;
-            }
-        } else {
-            echo "Not found!" . PHP_EOL;
-        }
-        break;
-    case 'user':
-        $service = (new ShowUserService());
-        $response = $service->execute(new ShowUserRequest($id));
-        $user = $response->user();
-
-        echo "====================" . PHP_EOL;
-        echo "->" . $user->username() . "<-" . PHP_EOL;
-        echo "Name: " . $user->name() . PHP_EOL;
-        echo "e-mail: " . $user->email() . PHP_EOL;
-        echo "City: " . $user->city() . PHP_EOL;
-        echo "Job: " . $user->companyName() . PHP_EOL;
-        echo "====================" . PHP_EOL;
-         break;
-    default:
-        echo "Invalid command!" . PHP_EOL;
-        break;
-}
+$console = new Console($argv);
+$console->run();
